b7cea676509786a36c8f621fdbb7d5178dc42cb7
pjha423 <103576919+pjha423@users.noreply.github.com>
2022-05-13 03:22:25 +0530

    Add files via upload
    Added Appium activities

---
+++ Appium/Activities/Activity5.java
@@ -0,0 +1,65 @@
+package Activity;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import org.aspectj.lang.annotation.After;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Activity5 {
+    AndroidDriver<MobileElement> driver;
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "<device name>");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("appPackage", "com.google.android.apps.messaging");
+        caps.setCapability("appActivity", ".ui.ConversationListActivity");
+        caps.setCapability("noReset", true);
+
+// Instantiate Appium Driver
+        URL appServer = new URL("http://0.0.0.0:4723/wd/hub");
+        driver = new AndroidDriver<MobileElement>(appServer, caps);
+    }
+
+    @Test
+    public void smsTest() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(MobileBy.AndroidUIAutomator("description(\"Start chat\")")).click();
+
+        String contactBoxLocator = "resourceId(\"com.google.android.apps.messaging:id/recipient_text_view\")";
+        Thread.sleep(2000);
+
+        driver.findElement(MobileBy.AndroidUIAutomator(contactBoxLocator)).sendKeys("7737602105");
+        driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+
+        Thread.sleep(2000);
+        driver.findElement(MobileBy.AndroidUIAutomator("resourceId(\"com.google.android.apps.messaging:id/compose_message_text\")")).sendKeys("Hello from Appium");
+
+        driver.findElement(MobileBy.AndroidUIAutomator("description(\"Send SMS\")")).click();
+
+        String messageLocator = "resourceId(\"com.google.android.apps.messaging:id/message_text\")";
+        Thread.sleep(2000);
+
+
+        // Assertion
+        String sentMessageText = driver.findElement(MobileBy.AndroidUIAutomator(messageLocator)).getText();
+        Assert.assertEquals(sentMessageText, "Hello from Appium");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        //driver.quit();
+    }
+}
---
+++ Appium/Activities/ScreenshotExample.java
@@ -0,0 +1,63 @@
+package Activity;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ScreenshotExample {
+    AndroidDriver<MobileElement> driver;
+
+    @BeforeClass
+    public void beforeClass() throws MalformedURLException {
+        // Set the Desired Capabilities
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "<Your device name>");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("appPackage", "com.android.calculator2");
+        caps.setCapability("appActivity", ".Calculator");
+        caps.setCapability("noReset", true);
+
+        // Instantiate Appium Driver
+        URL appServer = new URL("http://0.0.0.0:4723/wd/hub");
+        driver = new AndroidDriver<>(appServer, caps);
+    }
+
+    @Test
+    public void takeScreenshot() throws IOException {
+        // Take screenshot
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        // This specifies the location the screenshot will be saved
+        File screenShotName = new File("src/test/resources/screenshot.jpg");
+
+        // This will copy the screenshot to the file created
+        FileUtils.copyFile(scrFile, screenShotName);
+
+        // Set filepath for image
+        String filePath = "../" + screenShotName;
+
+        // Set image HTML in the report
+        String path = "<img src='" + filePath + "'/>";
+
+        // Show image in report
+        Reporter.log(path);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}

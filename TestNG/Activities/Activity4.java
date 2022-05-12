68fe58e552e9be865276d091da8b5eed69e9bf88
pjha423 <103576919+pjha423@users.noreply.github.com>
2022-05-13 03:19:28 +0530

    Add files via upload
    Added Test NG activities

---
+++ TestNG/Activities/Activity4.java
@@ -0,0 +1,17 @@
+package Activities;
+
+import org.testng.annotations.Test;
+
+public class Activity4 {
+    @Test
+    public void test1(){
+        System.out.println("I'm in first test");
+
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("I'm in second test");
+
+    }
+}
